d3dad730e66c240ced7f585a9af9863507ef0486
dgerbella <david_gerbella@yahoo.com>
2024-03-27 04:47:21 +0000

    Added Backup Storage private module

--- main.tf
+++ main.tf
@@ -201,7 +201,7 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
-module "backupstorage" {
+module "azure-backup" {
   source = "app.terraform.io/TERRAFORM_LAB_SD/azure-backup/azurerm"
   version = "1.0.2"
   resource_group_name = "${var.prefix}-private"
